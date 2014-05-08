4458e1d243a8330bf8012ff9d5bb4989793b310e
Lucas Sinclair <lucas.sinclair@me.com>
2014-05-08 18:52:26 +0200

    Fixing final details on plots for the methodology paper

--- scripts/sediment/sediment_plots.py
+++ scripts/sediment/sediment_plots.py
@@ -281,16 +281,50 @@ class UnifracNMDS(Graph):
         pyplot.close(fig)
 
 ################################################################################
+class PyroSorted(Graph):
+    """Sorted pyro samples"""
+    short_name = 'sorted_pyro'
+    width = 22.0
+    height = 14
+    bottom = 0.3
+    top = 0.95
+    left = 0.05
+    right = 0.95
+    formats = ('pdf', 'eps')
+
+    def plot(self):
+        # Data #
+        self.frame = self.parent.taxa_table.apply(lambda x: 100*x/x.sum(), axis=1)
+        # Rename #
+        self.frame = self.frame.reindex(index=sediment_new_names.keys())
+        self.frame = self.frame.rename(index=sediment_new_names)
+        # Plot #
+        fig = pyplot.figure()
+        axes = self.frame.plot(kind='bar', stacked=True, color=cool_colors)
+        fig = pyplot.gcf()
+        # Other #
+        axes.set_ylabel('Relative abundances in percent')
+        axes.xaxis.grid(False)
+        axes.yaxis.grid(False)
+        axes.set_ylim([0,100])
+        # Put a legend below current axis
+        axes.legend(loc='upper center', bbox_to_anchor=(0.5, -0.18), fancybox=True, shadow=True, ncol=5)
+        # Save it #
+        self.save_plot(fig, axes)
+        self.frame.to_csv(self.csv_path)
+        pyplot.close(fig)
+
+################################################################################
 if __name__ == '__main__':
     # Get the projects #
-    #proj = illumitag.projects['evaluation']
-    #proj.load()
-    #proj.graphs += [BarcodeStack(proj)]
-    #proj.graphs += [LengthDistribution(proj)]
-    #proj.graphs += [FractionTaxaBarStack(proj)]
-    #print "proj.graphs[-1].plot()"
-    #print "proj.graphs[-2].plot()"
-    #print "proj.graphs[-3].plot()"
+    proj = illumitag.projects['evaluation']
+    proj.load()
+    proj.graphs += [BarcodeStack(proj)]
+    proj.graphs += [LengthDistribution(proj)]
+    proj.graphs += [FractionTaxaBarStack(proj)]
+    print "proj.graphs[-1].plot()"
+    print "proj.graphs[-2].plot()"
+    print "proj.graphs[-3].plot()"
 
     # Get the cluster #
     cluster = illumitag.clustering.favorites.pyro_comparison
@@ -302,3 +336,9 @@ if __name__ == '__main__':
     unifrac.nmds.graph = UnifracNMDS(unifrac.nmds, base_dir=unifrac.nmds.base_dir)
     print "unifrac.nmds.run()"
     print "unifrac.nmds.graph.plot()"
+
+    # Sorted comparison taxa barstack #
+    phyla = cluster.otu_uparse.taxonomy_silva.comp_phyla
+    pyro_sorted = PyroSorted(phyla)
+    phyla.graphs += [pyro_sorted]
+    print "phyla.graphs[-1].plot()"
